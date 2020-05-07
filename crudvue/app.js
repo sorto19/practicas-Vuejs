9be552f3465c8d5a852c4e15951bdb2e172ec7d0
sorto19 <ramonsorto1@gmail.com>
2020-05-07 12:01:33 -0600

    crud con vue con localstorage

---
+++ crudvue/app.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+ el:'#app',
+ data:{
+     titulo:'Gym con Vue',
+     tareas:[],
+     nuevaTarea:''
+ },
+
+ methods:{
+     agregarTarea: function(){
+       this.tareas.push({
+            nombre:this.nuevaTarea,
+            estado:false
+        });
+        //console.log(this.tareas);
+        this.nuevaTarea='';
+        localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+
+     },
+     editarTarea:function(index){
+         //console.log('editar', index);
+         this.tareas[index].estado=true;
+        localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+
+     },
+     eliminar:function(index){
+         this.tareas.splice(index, 1);
+        localStorage.setItem('gym-vue',JSON.stringify(this.tareas));
+
+     }
+ },
+ created:function(){
+     let datosDB=JSON.parse(localStorage.getItem('gym-vue'));
+     if(datosDB === null){
+         this.tareas =[];
+     }else{
+         this.tareas=datosDB;
+     }
+ }
+});
