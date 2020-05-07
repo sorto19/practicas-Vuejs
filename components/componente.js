92b468c78ba2f88c4e21f279fd313cce0753542f
sorto19 <ramonsorto1@gmail.com>
2020-05-07 14:11:08 -0600

    component en vue

---
+++ components/componente.js
@@ -0,0 +1,16 @@
+Vue.coomponent('saludo',{
+    template://html
+    
+    `  <div>
+    <h1>{{saludo}}  </h1>
+    <h3>hcehjcuheu </h3>
+
+      </div>
+    `,
+    data(){
+        return{
+            saludo: 'saludo desde vue'
+        }
+    }
+
+});
